a0eb3a40291d5f8a3ccda16d5938a529c1a7dc69
Monvieux <l.sturm@free.fr>
2024-12-01 11:25:36 +0100

    Transform Code for All .csv Dataset

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,12 +5,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from exception import CustomException
-
 from logger import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTansformation
+from data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -30,7 +31,6 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
-
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initialited")
@@ -43,11 +43,15 @@ class DataIngestion:
             logging.info("Ingestion of the data is completed")
 
             return (self.ingestion_config.train_data_path,
-                    self.ingestion_config.test_data_path,
+                    self.ingestion_config.test_data_path
                     )
         except Exception as e:
             raise CustomException(e, sys)
-        
+
+#For Unit Test      
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTansformation()
+    data_transformation.initiate_data_transformation(train_data,test_data,"math_score")
